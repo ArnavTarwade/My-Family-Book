db68d17762535ae81078b8f8564f5f70acf0ac8b
ArnavTarwade <80450476+ArnavTarwade@users.noreply.github.com>
2021-08-23 15:48:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+  if(i == 9)
+    {
+     i=1;
+    }
+  
+  document.getElementById("family_member_image").src = images[i];
+i++;
+}
